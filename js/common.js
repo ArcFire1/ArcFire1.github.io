483f0e33448d1dfd88fd2e770d332048c6f18471
Alexei <storm371@gmail.com>
2018-08-05 18:20:28 +0300

    Update projects with Vue

--- js/common.js
+++ js/common.js
@@ -18,26 +18,49 @@ var portfolio = new Vue({
                                 name: "Beambox", 
                                 description: "Beambox is a website template with well balanced colors and a nice design.",
                                 image: "img/beambox.jpg",
-                                link: "#project-modal-1"
+                                link: "#project-modal-1",
+                                modalLink: "project-modal-1",
+                                modalImage: "img/beambox-full.png",
+                                demoLink: false,
+                                githubLink: "https://github.com/ArcFire1/beambox"
                         },
                         {
                                 name: "Limelight", 
                                 description: "This multipurpose website covers many possibilities, from blogging to showcasing portfolios.",
                                 image: "img/limelight.png",
-                                link: "#project-modal-2"
+                                link: "#project-modal-2",
+                                modalLink: "project-modal-2",
+                                modalImage: "img/limelight-full.png",
+                                demoLink: "http://l33654q5.beget.tech/",
+                                githubLink: "https://github.com/ArcFire1/limelight"
                         },
                         {
                                 name: "Ten-x", 
                                 description: "Dark landing page.",
                                 image: "img/ten-x.jpg",
-                                link: "#project-modal-3"
+                                link: "#project-modal-3",
+                                modalLink: "project-modal-3",
+                                modalImage: "img/ten-x-full.jpg",
+                                demoLink: "http://j25774hd.beget.tech/",
+                                githubLink: false
                         },
                         {
                                 name: "Beambox", 
                                 description: "Another landing page.",
                                 image: "img/seo.maza.png",
-                                link: "#project-modal-4"
+                                link: "#project-modal-4",
+                                modalLink: "project-modal-4",
+                                modalImage: "img/seo.maza-full.png",
+                                demoLink: false,
+                                githubLink: false
                         }
                 ]
+        },
+        methods: {
+                isLinkAvailable: function(){
+                        if (this.demoLink == "#") {
+
+                        }
+                }
         }
 });
